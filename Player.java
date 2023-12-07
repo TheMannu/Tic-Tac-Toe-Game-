4a53e14c3d38a0422da7d8fd80c1aee0c7e29c48
Ashwani Kumar <84488161+TheMannu@users.noreply.github.com>
2023-12-07 23:48:33 +0530

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,17 @@
+package TicTac;
+
+public abstract class Player {
+    String name;
+    char mark;
+
+    abstract void moveMaker();
+
+    boolean isValidMove(int row, int col) {
+        if (row >= 0 && row <= 2 && col >= 0 && col <= 2) {
+            if (TicTacToe.board[row][col] == ' ') {
+                return true;
+            }
+        }
+        return false;
+    }
+}
